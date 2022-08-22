4bb2b80e51e3dfff27dceea76bdb012025c85829
Isadora Hoelscher <isadorahoelscher@gmail.com>
2022-08-22 16:20:14 -0300

    adicionando as classes model

---
+++ src/main/java/Model/Fornecedor.java
@@ -0,0 +1,30 @@
+package Model;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "Produto")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fornecedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String cnpj;
+    private Boolean status;
+    @OneToMany
+    private ArrayList<Produto> produtos;
+}
+
+/*
+    {
+    "nome": "Vitão LTDA",
+    "cnpj": "1234567890",
+    "status": "true",
+    "produtos": [ ]
+    }
+ */
---
+++ src/main/java/Model/Produto.java
@@ -0,0 +1,34 @@
+package Model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Produto")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String familia;
+    private Boolean status;
+    private Tipo tipo;
+    private Date diaDaCompra;
+    private Date validade;
+    private Double preco;
+}
+
+/* {
+    "nome": "Girassol",
+    "familia": "Asteraceae",
+    "status": "true",
+    "tipo": "flor",
+    "diaDaCompra": "22/08/2022",
+    "validade": "27/08/2022",
+    "preco": "15.00",
+        } */
